8cc08ae74e7761789e5b21d149797ab627dc5688
wgova <webgster@yahoo.com>
2020-07-10 07:28:05 +0200

    add modules: correlation_analysis data_processers.py eda_plots.py

---
+++ correlation_analysis.py
@@ -0,0 +1,14 @@
+﻿Import seaborn as sns
+#----------------------- Correlation analyses-------------------------------------------
+def heatmap_numeric_w_dependent_variable(df, dependent_variable):
+    '''
+    Takes df, a dependant variable as str
+    Returns a heatmap of all independent variables' correlations with dependent variable 
+    '''
+    plt.figure(figsize=(8, 10))
+    g = sns.heatmap(df.corr()[[dependent_variable]].sort_values(by=dependent_variable), 
+                    annot=True, 
+                    cmap='coolwarm', 
+                    vmin=-1,
+                    vmax=1) 
+    return g
