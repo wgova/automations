9e1bacfc6afcf0fef6992a443488e87b592d911e
wgova <webgster@yahoo.com>
2020-07-13 07:02:06 +0200

    fix series object - shapiro_wilk

--- statistical_tests.py
+++ statistical_tests.py
@@ -3,22 +3,31 @@ from scipy.stats import shapiro, norm,kstest
 
 # normality test
 def shapiro_wilk_test(data, variable_to_test=None, alpha=0.5):
-
     """
+    H0: sample was drawn from a Gaussian distribution
     For a series x in data, calculates the Shapiro-Wilk statistic
     Return: statistic and p-value
     """
+    stat = []
+    p_value = []
+    sw_test = []
+    decision = []
     if variable_to_test == None:
         stat, p = shapiro(data)
-        print("Statistics=%.2f, p=%.2f" % (stat, p))
+        # print("Statistics=%.2f, p=%.2f" % (stat, p))
     else:
         stat, p = shapiro(data[variable_to_test])
     # interpret
     alpha = 0.05
     if p > alpha:
-        print("Sample likely Gaussian, fail to reject H0)")
+        decision.append("Gaussian")
+        # print("Sample likely Gaussian, fail to reject H0)")
     else:
-        print("Sample does not look Gaussian (reject H0)")
+        decision.append("Not Gaussian")
+        # print("Sample likely not Gaussian (reject H0)")
+    sw_test = pd.concat([stat.append(stat),p_value.append(),decision],
+        axis=1,keys=["statistic","p_value","Decision"])
+        
 
 # https://machinelearningmastery.com/a-gentle-introduction-to-normality-tests-in-python/
 # https://towardsdatascience.com/6-ways-to-test-for-a-normal-distribution-which-one-to-use-9dcf47d8fa93
