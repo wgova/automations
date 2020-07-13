157c99cbdc1f4fa68cdbc5264bee962f2998e98d
wgova <webgster@yahoo.com>
2020-07-13 07:11:00 +0200

    sw_test for all columns and return dataframe - shapiro_wilk

--- statistical_tests.py
+++ statistical_tests.py
@@ -1,4 +1,5 @@
 from scipy.stats import shapiro, norm,kstest
+import pandas as pd
 
 
 # normality test
@@ -12,22 +13,27 @@ def shapiro_wilk_test(data, variable_to_test=None, alpha=0.5):
     p_value = []
     sw_test = []
     decision = []
-    if variable_to_test == None:
-        stat, p = shapiro(data)
+    for col in data.columns:
+        try:
+            stat, p = shapiro(data[variable_to_test])
+        except:
+            pass
+        else:
+            stat, p = shapiro(data)
         # print("Statistics=%.2f, p=%.2f" % (stat, p))
-    else:
-        stat, p = shapiro(data[variable_to_test])
-    # interpret
-    alpha = 0.05
-    if p > alpha:
-        decision.append("Gaussian")
-        # print("Sample likely Gaussian, fail to reject H0)")
-    else:
-        decision.append("Not Gaussian")
+        stat.append(stat)
+        p_value.append()
+        # interpret
+        alpha = 0.05
+        if p > alpha:
+            decision.append("Gaussian")
+            # print("Sample likely Gaussian, fail to reject H0)")
+        else:
+            decision.append("Not Gaussian")
         # print("Sample likely not Gaussian (reject H0)")
     sw_test = pd.concat([stat.append(stat),p_value.append(),decision],
-        axis=1,keys=["statistic","p_value","Decision"])
-        
+    axis=1,keys=["statistic","p_value","Decision"])
+    return sw_test    
 
 # https://machinelearningmastery.com/a-gentle-introduction-to-normality-tests-in-python/
 # https://towardsdatascience.com/6-ways-to-test-for-a-normal-distribution-which-one-to-use-9dcf47d8fa93
