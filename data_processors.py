9723c6b1caa4b7598d754c288bbfdd7759509cf7
wgova <webgster@yahoo.com>
2020-08-09 19:29:24 +0200

    fix change_column_prefix

--- data_processors.py
+++ data_processors.py
@@ -29,9 +29,9 @@ def clean_header(df):
 
 def change_column_prefix(df, old_prefix="export_val__", new_prefix=None):
     if new_prefix == None:
-        [re.sub(f"^{old_prefix}", "", x) for x in df.columns]
+        df.columns = [re.sub(f"^{old_prefix}", "", x) for x in df.columns]
     else:
-        [re.sub(f"^{old_prefix}", new_prefix, x) for x in df.columns]
+        df.columns = [re.sub(f"^{old_prefix}", new_prefix, x) for x in df.columns]
     return df
 
 
