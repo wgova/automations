a6d3025e83afbd6801affe648867656b21b8034d
Webster Gova <webgster@yahoo.com>
2020-11-15 23:07:50 +0200

    bug fix func: clustering_helpers

--- clustering_helpers.py
+++ clustering_helpers.py
@@ -91,7 +91,7 @@ def plot_kmeans_clusters(data_array,number_of_clusters,name_of_data:str):
   plt.show()
   return idx
 
-def get_clustered_features(product_name,df_features,experiment):
+def get_clustered_features(product_name,df_features,experiment,PATH):
   #product_name = 'all_products'
   # Reduce dimensions using PCA
   pca = PCA(n_components=2)
