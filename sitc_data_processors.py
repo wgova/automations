03837094a30067a28cff0e7f5e48fbfbee50cd6c
Webster Gova <webgster@yahoo.com>
2022-01-29 18:30:55 +0200

    sitc data processors

--- sitc_data_processors.py
+++ sitc_data_processors.py
@@ -2,7 +2,11 @@ import numpy as np
 import pandas as pd
 
 def sitc_codes_to_sectors(df,sitc_code_column:'int64'):
-      conditions=[df[sitc_code_column]<990,
+    '''
+    Allocate sectors to SITC codes
+    sitc_code_column: integer codes
+    '''
+    conditions=[df[sitc_code_column]<990,
             df[sitc_code_column]<1223,
             df[sitc_code_column]<2930,
             df[sitc_code_column]<3511,
@@ -12,15 +16,8 @@ def sitc_codes_to_sectors(df,sitc_code_column:'int64'):
             df[sitc_code_column]<7940,
             df[sitc_code_column]<9000,
             df[sitc_code_column]<9710]
-  choices = ['food_animals',
-           'beverages_tobacco',
-           'crude_materials_inedibles',
-           'mineral_fuels_lubricants',
-           'animals_vegetable_oils',
-           'chemicals_products',
-           'manufactured_goods',
-           'machinery_and_transport',
-           'miscellaneous_manufactured_articles',
-           'commodities_and_other_transactions']
-  df['sectors'] = np.select(conditions,choices,default='miscellaneous_unspecified')
-  return df
+    choices = ['food_animals','beverages_tobacco','crude_materials_inedibles','mineral_fuels_lubricants',
+    'animals_vegetable_oils','chemicals_products','manufactured_goods','machinery_and_transport',
+    'miscellaneous_manufactured_articles','commodities_and_other_transactions']
+    df['sectors'] = np.select(conditions,choices,default='miscellaneous_unspecified')
+    return df
